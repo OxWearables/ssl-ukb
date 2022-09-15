3aeee794068cc834c6087183141e1d66f7c0e3bb
Gert Mertes <hey@gertmertes.be>
2022-09-15 16:03:10 +0100

    Add multiprocessing to make_capture24.py

--- utils/make_capture24.py
+++ utils/make_capture24.py
@@ -4,28 +4,54 @@ import os
 import numpy as np
 import pandas as pd
 import pathlib
-from tqdm.auto import tqdm
+from tqdm import tqdm
+from joblib import Parallel, delayed
 
+# change these to reflect the folders on your system
+DATA_DIR = '/data/UKBB/capture24/data/'  # location of Capture-24 (the .csv.gz files should be in this folder)
+OUT_DIR = '/data/UKBB/capture24_30hz_w30_o0/'  # output location of the processed dataset
+
+# number of CPU cores to use, need ~1.5GB memory per worker
+# don't set this higher than the number of physical cores
+NUM_WORKERS = 4
+
+# don't edit below this line (unless deliberately changing parameters)
 DEVICE_HZ = 100  # Hz
+RESAMPLE_HZ = 30  # Hz
 WINDOW_SEC = 30  # seconds
-# WINDOW_OVERLAP_SEC = 5  # seconds
 WINDOW_OVERLAP_SEC = 0  # seconds
 WINDOW_LEN = int(DEVICE_HZ * WINDOW_SEC)  # device ticks
 WINDOW_OVERLAP_LEN = int(DEVICE_HZ * WINDOW_OVERLAP_SEC)  # device ticks
 WINDOW_STEP_LEN = WINDOW_LEN - WINDOW_OVERLAP_LEN  # device ticks
 WINDOW_TOL = 0.01  # 1%
-DATAFILES = '/data/UKBB/capture24/data/P*.csv.gz'
-ANNOLABELFILE = '/data/UKBB/capture24/data/annotation-label-dictionary.csv'
-# OUTDIR = 'gertcapture24_100hz_w30_o5/'
-OUTDIR = 'gertcapture24_100hz_w30_o0_0t/'
-# LABEL = 'label:Willetts2018'
+DATAFILES = os.path.join(DATA_DIR, 'P*.csv.gz')
+ANNOLABELFILE = os.path.join(DATA_DIR, 'annotation-label-dictionary.csv')
 LABEL = 'label:Walmsley2020'
 
-pathlib.Path(OUTDIR).mkdir(parents=True, exist_ok=True)
+annolabel = pd.read_csv(ANNOLABELFILE, index_col='annotation')
+
+
+def resize(x, length, axis=1):
+    """Resize the temporal length using linear interpolation.
+    X must be of shape (N,M,C) (channels last) or (N,C,M) (channels first),
+    where N is the batch size, M is the temporal length, and C is the number
+    of channels.
+    If X is channels-last, use axis=1 (default).
+    If X is channels-first, use axis=2.
+    """
+    from scipy.interpolate import interp1d
+
+    length_orig = x.shape[axis]
+    t_orig = np.linspace(0, 1, length_orig, endpoint=True)
+    t_new = np.linspace(0, 1, length, endpoint=True)
+    x = interp1d(t_orig, x, kind="linear", axis=axis, assume_sorted=True)(
+        t_new
+    )
+    return x
 
 
 def is_good_quality(w):
-    ''' Window quality check '''
+    """ Window quality check """
 
     if w.isna().any().any():
         return False
@@ -45,11 +71,9 @@ def is_good_quality(w):
     return True
 
 
-annolabel = pd.read_csv(ANNOLABELFILE, index_col='annotation')
+def make(datafile):
+    X, Y, T, P, = [], [], [], []
 
-X, Y, T, P, = [], [], [], []
-
-for datafile in tqdm(glob.glob(DATAFILES)):
     data = pd.read_csv(datafile, parse_dates=['time'], index_col='time',
                        dtype={'x': 'f4', 'y': 'f4', 'z': 'f4', 'annotation': 'str'})
 
@@ -70,17 +94,38 @@ for datafile in tqdm(glob.glob(DATAFILES)):
         T.append(t)
         P.append(p)
 
-X = np.asarray(X)
-Y = np.asarray(Y)
-T = np.asarray(T)
-P = np.asarray(P)
+    X = np.asarray(X)
+    Y = np.asarray(Y)
+    T = np.asarray(T)
+    P = np.asarray(P)
 
-np.save(os.path.join(OUTDIR, 'X'), X)
-np.save(os.path.join(OUTDIR, 'Y'), Y)
-np.save(os.path.join(OUTDIR, 'time'), T)
-np.save(os.path.join(OUTDIR, 'pid'), P)
+    if DEVICE_HZ != RESAMPLE_HZ:
+        X = resize(X, int(RESAMPLE_HZ * WINDOW_SEC))
 
-print(f"Saved in {OUTDIR}")
-print("X shape:", X.shape)
-print("Y distribution:")
-print(pd.Series(Y).value_counts())
+    return X, Y, T, P
+
+
+if __name__ == '__main__':
+    pathlib.Path(OUT_DIR).mkdir(parents=True, exist_ok=True)
+
+    x, y, t, p = zip(
+        *Parallel(n_jobs=NUM_WORKERS)(
+            delayed(make)(datafile)
+            for datafile in tqdm(glob.glob(DATAFILES))
+        )
+    )
+
+    X = np.vstack(x)
+    Y = np.hstack(y)
+    T = np.hstack(t)
+    P = np.hstack(p)
+
+    np.save(os.path.join(OUT_DIR, 'X'), X)
+    np.save(os.path.join(OUT_DIR, 'Y_Walmsley'), Y)
+    np.save(os.path.join(OUT_DIR, 'time'), T)
+    np.save(os.path.join(OUT_DIR, 'pid'), P)
+
+    print(f"Saved in {OUT_DIR}")
+    print("X shape:", X.shape)
+    print("Y distribution:")
+    print(pd.Series(Y).value_counts())
